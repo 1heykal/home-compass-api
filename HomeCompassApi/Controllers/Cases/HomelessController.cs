aaad5e43ed15abe6deb0791cfebc4e1b7c52d504
Osama Heykal <oneheykal@gmail.com>
2024-01-26 22:16:02 +0200

    added controller

---
+++ HomeCompassApi/Controllers/Cases/HomelessController.cs
@@ -0,0 +1,33 @@
+﻿using HomeCompassApi.BLL;
+using HomeCompassApi.Models.Cases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCompassApi.Controllers.Cases
+{
+    public class HomelessController : Controller
+    {
+        private readonly IRepository<Homeless> _repository;
+        public HomelessController(IRepository<Homeless> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Homeless> Get(int id)
+        {
+            var homeless = _repository.GetById(id);
+            if (homeless is null)
+                return NotFound();
+
+            return homeless;
+        }
+
+
+    }
+}
